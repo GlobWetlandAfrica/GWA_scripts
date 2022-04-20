86656976fb23c8b8278b91260996a3503623ca35
Radoslaw Guzinski <radosuav@op.pl>
2022-04-20 10:20:52 +0200

    Fix post-classification comparison script

--- scripts/GWA_TBX/postclassificationComparison.py
+++ scripts/GWA_TBX/postclassificationComparison.py
@@ -1,7 +1,7 @@
 import os
-from processing.tools.system import tempHelpFolder
-from processing.core.ProcessingLog import ProcessingLog
 
+from processing.tools.system import tempHelpFolder
+from qgis import processing
 from qgis.processing import alg
 
 @alg(
@@ -10,8 +10,8 @@ from qgis.processing import alg
     group="ts",
     group_label=alg.tr("Timeseries")
 )
-@alg.input(type=alg.RASTER_LAYER, name="classification", label="Select classification of \"initial state\"", default=False)
-@alg.input(type=alg.RASTER_LAYER, name="reference", label="Select classification of \"final state\"", default=False)
+@alg.input(type=alg.RASTER_LAYER, name="classification", label="Select classification of \"initial state\"")
+@alg.input(type=alg.RASTER_LAYER, name="reference", label="Select classification of \"final state\"")
 @alg.input(type=bool, name='w', label='Wide report (132 columns)', default=False)
 @alg.input(type=alg.EXTENT, name='extent', label='Region extent')
 @alg.input(type=alg.FILE_DEST, name='output', label='Name for output file containing the change detection matrix')
@@ -19,8 +19,8 @@ def pg04waterqualityparameters04olcicrsselect(instance, parameters, context, fee
     """
     pg04waterqualityparameters04olcicrsselect
     """
-    classification = instance.parameterAsString(parameters, 'classification', context)
-    reference = instance.parameterAsString(parameters, 'reference', context)
+    classification = instance.parameterAsRasterLayer(parameters, 'classification', context).source()
+    reference = instance.parameterAsRasterLayer(parameters, 'reference', context).source()
     w = instance.parameterAsString(parameters, 'w', context)
     extent = instance.parameterAsString(parameters, 'extent', context)
     output = instance.parameterAsString(parameters, 'output', context)
@@ -35,8 +35,16 @@ def pg04waterqualityparameters04olcicrsselect(instance, parameters, context, fee
     tempOutput = tempHelpFolder() + os.sep + "postclassificationComparisionScript.txt"
     if os.path.exists(tempOutput):
         os.remove(tempOutput)
-
-    if processing.runalg("grass7:r.kappa", classification,reference,'CHANGE DETECTION MATRIX', True, w, extent, tempOutput):
+    
+    params = {"classification": classification,
+              "reference": reference,
+              "title": "CHANGE DETECTION MATRIX",
+              "-h": True,
+              "-w": w,
+              "output": tempOutput,
+              "GRASS_REGION_PARAMETER": extent}
+              
+    if processing.run("grass7:r.kappa", params, is_child_algorithm=True, context=context, feedback=feedback):
         with open(tempOutput) as inputFile, open(output, "a") as outputFile:
             lines = inputFile.readlines()
             writeLines = False
@@ -49,9 +57,5 @@ def pg04waterqualityparameters04olcicrsselect(instance, parameters, context, fee
                     writeLines = True
                     outputFile.write('Change detection matrix\n')
         feedback.setProgressText('Saved change detection matrix to file %s' % output)
-        loglines.append('Saved change detection matrix to file %s' % output)
-        ProcessingLog.addToLog(ProcessingLog.LOG_INFO, loglines)
     else:
         feedback.setProgressText('ERROR running r.kappa. Check log for details.')
-        loglines.append('ERROR running r.kappa. Check log for details.')
-        ProcessingLog.addToLog(ProcessingLog.LOG_INFO, loglines)
